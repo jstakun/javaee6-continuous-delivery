dbe7c8ab982c578514f31f95f0ef443fde793c21
demo <demo@localhost>
2015-01-14 14:49:23 +0100

    Test mock browser

--- src/test/java/org/javaee7/sample/ui/LoginScreenSeleniumTest.java
+++ src/test/java/org/javaee7/sample/ui/LoginScreenSeleniumTest.java
@@ -26,8 +26,8 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Filters;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
@@ -61,8 +61,6 @@ public class LoginScreenSeleniumTest {
                 new StringAsset("<faces-config version=\"2.0\"/>"),
                 "faces-config.xml");
     }
-    
-    //Uncomment to run selenium browser test
     
     @Drone
     DefaultSelenium browser;
